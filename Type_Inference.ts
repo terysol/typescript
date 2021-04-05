b67f60d397fdcd8aa10adc507ea5bdcd5eb6986b
Solmin Kim <solminkim03@naver.com>
2021-04-05 13:53:59 +0900

    21.04.05 _ 타입추론

---
+++ Type_Inference.ts
@@ -0,0 +1,18 @@
+let a=10;
+
+a=10;
+
+// 타입추론 변수 a의 타입은 int형이 된다. 
+
+let student={
+    name:'Jake',
+    course : 'Getting started with ts',
+    codingIQ:80,
+    code:function(){
+        console.log('typescript');
+    }
+}
+
+function calculateCoddingIQ(lostPoint:any){
+    return 100-lostPoint;
+}// js 결과물이 숫자가 리턴한다는 것을 알려줌. 타입추론
