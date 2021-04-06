26da447c72407d6e1e6b9a041708316e26c3c3b4
Solmin Kim <solminkim03@naver.com>
2021-04-06 20:57:54 +0900

    21.04.06 _ 타입명시

--- Type.js
+++ Type.js
@@ -13,3 +13,12 @@ var student = {
 function calculateCoddingIQ(lostPoint) {
     return 100 - lostPoint;
 } // js 결과물이 숫자가 리턴한다는 것을 알려줌. 타입추론
+// Syntac 타입 명시
+var studentID = 12345;
+var studentName = 'Jenny Kim';
+var age = 21;
+function getStudentDetails(studentID) {
+    // : ~   ==> 리턴값 타입명시 , void는 아무것도 반환하지 않는다
+    return null;
+}
+// type에 대한 정보가 많을 수록 ts는 코드를 효과적으로 짤 수 있음.
---
+++ Type.ts
@@ -0,0 +1,34 @@
+let a=10;
+
+a=10;
+
+// 타입추론 변수 a의 타입은 int형이 된다. 
+
+let student={
+    name:'Jake',
+    course : 'Getting started with ts',
+    codingIQ:80,
+    code:function(){
+        console.log('typescript');
+    }
+}
+
+function calculateCoddingIQ(lostPoint:any){
+    return 100-lostPoint;
+}// js 결과물이 숫자가 리턴한다는 것을 알려줌. 타입추론
+
+// Syntac 타입 명시
+let studentID:number=12345;
+let studentName : string='Jenny Kim';
+let age:number=21;
+ 
+function getStudentDetails(studentID:number):{
+    studentID:number;           // 반환되는 객체를 구조를 명시 
+    studentName:string;
+    age:number;
+}{
+    // : ~   ==> 리턴값 타입명시 , void는 아무것도 반환하지 않는다
+    return null as any;
+}
+// type에 대한 정보가 많을 수록 ts는 코드를 효과적으로 짤 수 있음.
+
